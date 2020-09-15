f55429225db7ef7eff2a226c06aaaf694e2d6ec3
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-09-15 09:48:58 -0300

    Escrevendo teste

--- teste/src/application/Program.java
+++ teste/src/application/Program.java
@@ -4,7 +4,16 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		String nome = "Eduardo";
+		String nome1 = "Eduardo";
+		String nome3 = "Maria";
 		
+		if (nome.equals(nome3)) {                                // compara o conteudo das strings equals()
+			System.out.println("Nome igual");
+		}
+		else {
+			System.out.println("Nome diferente");
+		}
 
 	}
 
