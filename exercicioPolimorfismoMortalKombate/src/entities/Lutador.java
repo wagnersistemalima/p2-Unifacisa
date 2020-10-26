298e0f1bf2469f34c764b1da2eb970a1d14153c0
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-10-26 13:17:54 -0300

    Update

--- exercicioPolimorfismoMortalKombate/src/entities/Lutador.java
+++ exercicioPolimorfismoMortalKombate/src/entities/Lutador.java
@@ -2,18 +2,26 @@ package entities;
 
 public class Lutador {
 	
+	// atributos basicos
+	
 	protected String nome;
 	protected Integer vida;
+	
+	// construtor padrão
 	
 	public Lutador() {
 		
 	}
+	
+	// construtor com argumentos
 
 	public Lutador(String nome) {
 		super();
 		this.nome = nome;
 		this.vida = 100;
 	}
+	
+	// Getters & setters
 
 	public String getNome() {
 		return nome;
@@ -32,8 +40,42 @@ public class Lutador {
 	}
 	
 	// metodo socar
-	public void socar() {
-		
+	
+	public void socar(Lutador lutador1) {
+		if (lutador1 != null) {
+			lutador1.vida -= 7;
+		}
 	}
+	
+	// metodo chutar
+	
+	public void chutar(Lutador lutador1) {
+		if (lutador1 != null) {
+			lutador1.vida -= 5;
+		}
+	}
+	
+	// metodo atirar
+	
+	public void atirar(Lutador lutador1) {
+		if (lutador1 != null) {
+			lutador1.vida -= 10;
+		}
+	}
+	
+	// metodo lançar magia
+	
+	public void magia(Lutador lutador1) {
+		if (lutador1 != null) {
+			lutador1.vida -= 12;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Lutador [nome=" + nome + ", vida=" + vida + "]";
+	}
+	
+	
 
 }
